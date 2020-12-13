b3f842ec48c09d16511fc24ace74d8bd9aa87401
sujang Jo <46040847+josoooooooooojang@users.noreply.github.com>
2020-12-13 19:36:58 +0900

    Add : ImageTool 생성

---
+++ ImageTool/ImageTool.h
@@ -0,0 +1,39 @@
+﻿
+// ImageTool.h: ImageTool 응용 프로그램의 기본 헤더 파일
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // 주 기호입니다.
+
+
+// CImageToolApp:
+// 이 클래스의 구현에 대해서는 ImageTool.cpp을(를) 참조하세요.
+//
+
+class CImageToolApp : public CWinAppEx
+{
+public:
+	CImageToolApp() noexcept;
+
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 구현입니다.
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CImageToolApp theApp;
